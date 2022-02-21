91e4eef2a072695ca0ad92ca7743320b16a13d94
ajaykumaray <44997886+ajaykumaray@users.noreply.github.com>
2022-02-21 15:39:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,14 @@
 provider "aws" {
-  region     = "ap-northeast-1"
-  access_key = "AKIA5WKYNKMSTQNLJ7D5"
-  secret_key = "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"
+  region = "us-east-1"
 }
 
-data "aws_ami" "ami-050a677a0dbb257ab" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["941320131365"] # Canonical
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIA5WKYNKMS7VQI7U5Z"
+  secret_key = "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"
 }
 
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+resource "aws_insatnce" "firstec2" {
+  ami = "ami-0341aeea105412b57"
+  instance_type = "t2.micro"
 }
